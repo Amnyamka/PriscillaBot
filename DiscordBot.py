cbc6dfc86e613b8d468b11a1df1820045aa541a8
Irving McAllister <Server100@list.ru>
2020-10-08 17:04:14 +0300

    version 0.2

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,15 +2,14 @@ import discord
 from discord.ext import commands
 
 
-TOKEN = 'NjY3Nzg2MTk4Mzc4NjEwNjk4.XiHyXA.vnl17HBFjtObh4kBGejhnCvhVRo'
+
 bot = commands.Bot(command_prefix='!')
 
-players = {}
+TOKEN = ''
 
-
-@bot.command(pass_context=True)  # разрешаем передавать агрументы
-async def test(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send('Hello boi!')  # отправляем обратно аргумент
+@bot.command(pass_context=True)  # allow the transfer of arguments
+async def test(ctx, arg):  # create an asynchronous bot function
+    await ctx.send('Hello boi!')  # send back the argument
 
 
 @bot.event
@@ -23,69 +22,101 @@ async def on_member_join(member):
 
 @bot.event
 async def on_ready():
-    print("Запущен как:")
+    print("Launched as:")
     print(bot.user.name)
     print(bot.user.id)
+    print('Version: 0.2')
     print('---------------')
 
+@bot.command(pass_context=True)
+async def role(ctx, userrole, *,user: discord.Member):                                                                  # checking the role of the user
+                                                                                                                        # Example: !role role Amnyam
+                                                                                                                        # Use a roles only with 1 word or it will NOT work.username = discord.Membe
 
-@commands.has_permissions(administrator=True)
-@bot.command(pass_context = True)
-async def dm(ctx, *, ctx2):
-    await ctx.send(ctx2)
+#Amnyam --> Amnyam#2062 need more time to solve this. I know the problem with Member "" not found.
+    try:
+        role = discord.utils.find(lambda r: r.name == userrole, ctx.message.guild.roles)
+
+        if role in user.roles:
+            await ctx.send("{} has role: {} :white_check_mark:".format(user, role))
+            print("{} has role: {}".format(user, role))
+            print('==========================')
+
+        elif role == None:
+            await ctx.send("There is no such role: {} :no_entry:".format(userrole))
+            print("There is no such role: {}".format(userrole))
+            print('==========================')
+
+        else:
+            await ctx.send('{} has no role: {} :no_entry_sign:'.format(user, role))
+            print('{} has no role: {}'.format(user, role))
+            print('==========================')
+    except:
+        await ctx.send('There is no such user :no_entry_sign:')
+        print('There is no such user')
+        print('==========================')
 
 @bot.command(pass_context=True)
-@commands.has_role("dumbass")
-async def role(ctx, *, user: discord.Member):
-    role = discord.utils.find(lambda r: r.name == 'dumbass', ctx.message.guild.roles)
-    if role in user.roles:
-        await ctx.send("{} имеет роль {}".format(user, role))
-        print(role)
-    else:
-        await ctx.send('{} не имеет роль {}'.format(user, role))
-
-@bot.command(pass_context = True)
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    await bot.join_voice_channel(channel)
-
-@bot.command(pass_context = True)
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = bot.voice_client_in(server)
-    await voice_client.disconnect()
-
-
-@bot.command(pass_context = True)
-async def play(ctx, url):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    player = await voice_client.create_ytdl_player(url)
-    players[server.id] = player
-    player.start()
-
-
-@commands.has_permissions(administrator=True)
-@bot.command(pass_context = True)
-async def send(ctx, rolem, *,content):
+async def roleall(ctx, userrole):                                                                                       # checking the role of all users
+                                                                                                                        # Example: !roleall role
     x = ctx.message.guild.members
-    print (await ctx.guild.fetch_roles())
     for ctx.message.id in x:
         user = ctx.message.id
 
-        role = discord.utils.find(lambda r: r.name == rolem, ctx.message.guild.roles)
+
+        role = discord.utils.find(lambda r: r.name == userrole, ctx.message.guild.roles)
+        if role in user.roles:
+            await ctx.send("{} has role: {} :white_check_mark:".format(user, role))
+            print("{} has role: {}".format(user, role))
+            print('==========================')
+
+        elif role == None:
+            await ctx.send("There is no such role: {} :no_entry:".format(userrole))
+            print("There is no such role: {}".format(userrole))
+            print('==========================')
+            break
+
+        # else:                                                                                                         # Use this if you want to see people without a specified role
+        #     await ctx.send('{} has no role: {} :no_entry_sign:'.format(user, role))
+        #     print('{} has no role: {}'.format(user, role))
+        #     print('==========================')
+
+
+
+
+@commands.has_permissions(administrator=True)
+@bot.command(pass_context = True)
+async def send(ctx, userrole, *,content):                                                                               # sends direct messages to all users of the group with the specified role.
+                                                                                                                        # Example: !send role Text
+                                                                                                                        # Use a roles only with 1 word or it will NOT work.
+    x = ctx.message.guild.members
+
+    for ctx.message.id in x:
+
+        user = ctx.message.id
+        role = discord.utils.find(lambda r: r.name == userrole, ctx.message.guild.roles)
         if role in user.roles:
             try:
                 await ctx.message.id.send(content)
-                await ctx.send("Сообщение отправлено : {} :white_check_mark: ".format(ctx.message.id))
+                await ctx.send("Message delivered : {} :white_check_mark: ".format(user))
+                print("Message delivered : {}".format(user))
+                print('==========================')
 
             except:
-                print("ОШИБКА!")
-                await ctx.send("Не могу отправить : {} :no_entry: ".format(ctx.message.id))
-                print(ctx.message.id)
-                print(x)
+                await ctx.send("ERROR! Can't send the message : {} :no_entry: ".format(user))
+                print("ERROR! Can't send the message : {}".format(user))
+                print('==========================')
+
+
+        elif role == None:
+            await ctx.send("There is no such role: {} :no_entry:".format(userrole))
+            print("There is no such role: {}".format(userrole))
+            print('==========================')
+            break
 
         else:
-            await ctx.send("{} не имеет роли {} :no_entry_sign: ".format(ctx.message.id, role))
+            await ctx.send("{} has no role {} :no_entry_sign: ".format(user, role))
+            print("{} has no role {} :no_entry_sign: ".format(user, role))
+            print('==========================')
 
 bot.run(TOKEN)
